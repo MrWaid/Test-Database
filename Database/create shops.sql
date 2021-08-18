0255e7fcce76d565e349f4fbd428b66853ca3c04
Alexandra Savenkova <44192340+MrWaid@users.noreply.github.com>
2021-08-18 19:18:30 +0300

    Create create shops.sql

---
+++ Database/create shops.sql
@@ -0,0 +1,10 @@
+create table "Shops"
+(
+    id   integer,
+    name text
+);
+
+insert into "Shops" (id, name)
+    values (1, 'Обожжённый молот'),
+           (2, 'Заложенная креветка'),
+           (3, 'Элексиры Элгрима');
